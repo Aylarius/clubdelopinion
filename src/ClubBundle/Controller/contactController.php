861516d692287956c851b13be17097bca38793b0
Thifaine <thifaine.noirault@gmail.com>
2016-10-11 17:05:03 +0200

    creation_page_contact

---
+++ src/ClubBundle/Controller/contactController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace ClubBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use ClubBundle\Entity\contact;
+use ClubBundle\Form\contactType;
+
+/**
+ * contact controller.
+ *
+ */
+class contactController extends Controller
+{
+    /**
+     * Lists all contact entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $contacts = $em->getRepository('c')->findAll();
+
+        return $this->render('contact/index.html.twig', array(
+            'contacts' => $contacts,
+        ));
+    }
+
+    /**
+     * Creates a new contact entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $contact = new contact();
+        $form = $this->createForm('ClubBundle\Form\contactType', $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush();
+
+            return $this->redirectToRoute('contact_show', array('id' => $contact->getId()));
+        }
+
+        return $this->render('contact/new.html.twig', array(
+            'contact' => $contact,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a contact entity.
+     *
+     */
+    public function showAction(contact $contact)
+    {
+        $deleteForm = $this->createDeleteForm($contact);
+
+        return $this->render('contact/show.html.twig', array(
+            'contact' => $contact,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing contact entity.
+     *
+     */
+    public function editAction(Request $request, contact $contact)
+    {
+        $deleteForm = $this->createDeleteForm($contact);
+        $editForm = $this->createForm('ClubBundle\Form\contactType', $contact);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush();
+
+            return $this->redirectToRoute('contact_edit', array('id' => $contact->getId()));
+        }
+
+        return $this->render('contact/edit.html.twig', array(
+            'contact' => $contact,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a contact entity.
+     *
+     */
+    public function deleteAction(Request $request, contact $contact)
+    {
+        $form = $this->createDeleteForm($contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($contact);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('contact_index');
+    }
+
+    /**
+     * Creates a form to delete a contact entity.
+     *
+     * @param contact $contact The contact entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(contact $contact)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('contact_delete', array('id' => $contact->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/ClubBundle/Form/contactType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ClubBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class contactType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('soustitre')
+            ->add('paragraph')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ClubBundle\Entity\contact'
+        ));
+    }
+}
