5ab1066a631a4cbd5012647e49432575d473330c
VERO56 <veronique.lejeune.56@gmail.com>
2016-10-11 17:10:58 +0200

    ajout page av3

---
+++ src/ClubBundle/Form/av3Type.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ClubBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class av3Type extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titleAv3')
+            ->add('paragraphAv3')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ClubBundle\Entity\av3'
+        ));
+    }
+}
