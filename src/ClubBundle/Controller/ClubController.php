0e6584a29de68c87a25d9930b543726fd3027d4e
Thifaine <thifaine.noirault@gmail.com>
2016-11-02 10:48:11 +0100

    Ajout pages supp sur edit page d'accueil

--- src/ClubBundle/Controller/ClubController.php
+++ src/ClubBundle/Controller/ClubController.php
@@ -6,7 +6,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use ClubBundle\Entity\Club;
+use ClubBundle\Entity\club_sup;
+use ClubBundle\Entity\av3;
+use ClubBundle\Entity\EvSup;
 use ClubBundle\Form\ClubType;
+use ClubBundle\Form\av3Type;
+use ClubBundle\Form\club_supType;
+use ClubBundle\Form\EvSupType;
+
 
 /**
  * Club controller.
@@ -84,15 +91,76 @@ class ClubController extends Controller
 
             return $this->redirectToRoute('club_edit', array('id' => $club->getId()));
         }
-        $em = $this->getDoctrine()->getManager();
 
+        /* Récupération des données de l'entité Club_Sup */
+        $club_sup_Em = $this->getDoctrine()->getManager();
+        $club_sups = $club_sup_Em->getRepository('ClubBundle:club_sup')->findAll();
+
+        /* Formulaire Création Page Club_Sup */
+        $club_sup = new club_sup();
+        $newForm = $this->createForm('ClubBundle\Form\club_supType', $club_sup);
+        $newForm->handleRequest($request);
+
+        if ($newForm->isSubmitted() && $newForm->isValid()) {
+            $club_sup_Em = $this->getDoctrine()->getManager();
+            $club_sup_Em->persist($club_sup);
+            $club_sup_Em->flush();
+
+            return $this->redirectToRoute('club_edit', array('id' => $club->getId()));
+        }
+
+        /* Récupération des données de l'entité Av3 */
+        $av3_Em = $this->getDoctrine()->getManager();
+        $av3s = $av3_Em->getRepository('ClubBundle:av3')->findAll();
+
+        /* Formulaire Création Page Av3 */
+        $av3 = new av3();
+        $newForm2 = $this->createForm('ClubBundle\Form\av3Type', $av3);
+        $newForm2->handleRequest($request);
+
+        if ($newForm2->isSubmitted() && $newForm2->isValid()) {
+            $av3_Em = $this->getDoctrine()->getManager();
+            $av3_Em->persist($av3);
+            $av3_Em->flush();
+
+            return $this->redirectToRoute('club_edit', array('id' => $club->getId()));
+        }
+
+        /* Récupération des données de l'entité EvSup */
+        $evsup_Em = $this->getDoctrine()->getManager();
+        $evSups = $evsup_Em->getRepository('ClubBundle:EvSup')->findAll();
+
+        /* Formulaire Création Page EvSup */
+        $evSup = new Evsup();
+        $newForm3 = $this->createForm('ClubBundle\Form\EvSupType', $evSup);
+        $newForm3->handleRequest($request);
+
+        if ($newForm3->isSubmitted() && $newForm3->isValid()) {
+            $evsup_Em = $this->getDoctrine()->getManager();
+            $evsup_Em->persist($evSup);
+            $evsup_Em->flush();
+
+            return $this->redirectToRoute('club_edit', array('id' => $club->getId()));
+        }
+
+        /* Récupération des données de l'entité Club pour afficher la barre de navigation */
+        $em = $this->getDoctrine()->getManager();
         $clubs = $em->getRepository('ClubBundle:Club')->findAll();
 
         return $this->render('ClubBundle:club:edit.html.twig', array(
             'club' => $club,
             'clubs' => $clubs,
+            'club_sups' => $club_sups,
+            'club_sup' => $club_sup,
+            'evSups' => $evSups,
+            'evSup' => $evSup,
+            'av3s' => $av3s,
+            'av3' => $av3,
             'edit_form' => $editForm->createView(),
             'delete_form' => $deleteForm->createView(),
+            'newForm' => $newForm->createView(),
+            'newForm2' => $newForm2->createView(),
+            'newForm3' => $newForm3->createView(),
         ));
     }
 
